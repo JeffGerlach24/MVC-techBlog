eb4bb940e5ea3340560d639ba45c7af61d2ce4ba
Jeff Gerlach <jeff.gerlach2@gmail.com>
2022-02-27 15:11:25 -0600

    working on java.

---
+++ public/JavaScript/postHandler.js
@@ -0,0 +1,27 @@
+async function createPostHandler(event) {
+    event.preventDefault();
+
+    document.location.replace('/dashboard/new')
+}
+
+document.querySelector('#create-new-post').addEventListener('click', createPostHandler);
+
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${post_id}`, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
